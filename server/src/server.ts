aa81623177287379fc84f67f94c66de22abb7d09
calasso <rcalasso@gmail.com>
2022-11-04 16:28:31 -0300

    Aula 2 - NLW-Copa

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,20 @@
-import Fastify from "fastify";
+import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from  '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
   log: ['query'],
 })
 
-async function bootstrap() {
+async function booststrap() {
   const fastify = Fastify({
-    logger: true,
+    // Fastify vai soltando logs de tudo que está acontecendo com a aplicação
+    logger: true
   })
 
+  // Permite que qualquer aplicação acesse o nosso back-end
   await fastify.register(cors, {
     origin: true,
   })
@@ -18,21 +22,31 @@ async function bootstrap() {
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
 
-    /*
-      const pools = await prisma.pool.findMany({
-        where: {
-          code: {
-            startsWith: 'R'
-          }
-        }
-      })
-    */
-
-
     return { count }
   })
 
-  await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ })
+  fastify.post('/pools', async (request, reply) => {
+    // Validação com o zod
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+
+    const generate = new ShortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      }
+    })
+
+    return reply.status(201).send({ code })
+  })
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
-bootstrap()
+booststrap()
